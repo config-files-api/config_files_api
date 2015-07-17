64efd151999a0175e3168d96909182b99b9665c5
Josef Reidinger <jreidinger@suse.cz>
2015-07-17 09:07:32 +0200

    fix grub model to use new augeas parser

--- lib/config_files/grub_model.rb
+++ lib/config_files/grub_model.rb
@@ -1,48 +1,36 @@
-require "config_file/base_model"
-require "config_file/key_value_parser"
+require "config_files/base_model"
+require "config_files/augeas_parser"
 
 module ConfigFiles
-  PARSER = KeyvalueParser
+  PARSER = AugeasParser.new("sysconfig.lns")
   PATH = "/etc/default/grub"
 
   class GrubModel < BaseModel
     def initialize(file_class: File)
       super(PARSER, PATH, file_class: file_class)
-      self.data = {}
+      self.data = AugeasTree.new
     end
 
     def os_prober_enabled?
       return true unless data["GRUB_DISABLE_OS_PROBER"]
-      return data["GRUB_DISABLE_OS_PROBER"][:value] != "true"
+      return data["GRUB_DISABLE_OS_PROBER"] != "true"
     end
 
     def disable_os_prober
-      data["GRUB_DISABLE_OS_PROBER"] ||= {}
-      data["GRUB_DISABLE_OS_PROBER"][:value] = "true"
-      data["GRUB_DISABLE_OS_PROBER"][:commented_out] = false
+      data["GRUB_DISABLE_OS_PROBER"] = "true"
     end
 
     def enable_os_prober
-      data["GRUB_DISABLE_OS_PROBER"] ||= {}
-      data["GRUB_DISABLE_OS_PROBER"][:value] = "false"
-      data["GRUB_DISABLE_OS_PROBER"][:commented_out] = false
+      data["GRUB_DISABLE_OS_PROBER"] = "false"
     end
 
     def disable_recovery_entry
-      data["GRUB_DISABLE_RECOVERY"] ||= {}
-      data["GRUB_DISABLE_RECOVERY"][:value] = "true"
-      data["GRUB_DISABLE_RECOVERY"][:commented_out] = false
-      data["GRUB_CMDLINE_LINUX_RECOVERY"] ||= {}
-      data["GRUB_CMDLINE_LINUX_RECOVERY"][:commented_out] = true
+      data["GRUB_DISABLE_RECOVERY"] = "true"
     end
 
     def enable_recovery_entry(kernel_params)
-      data["GRUB_DISABLE_RECOVERY"] ||= {}
-      data["GRUB_DISABLE_RECOVERY"][:value] = "false"
-      data["GRUB_DISABLE_RECOVERY"][:commented_out] = false
-      data["GRUB_CMDLINE_LINUX_RECOVERY"] ||= {}
-      data["GRUB_CMDLINE_LINUX_RECOVERY"][:value] = kernel_params
-      data["GRUB_CMDLINE_LINUX_RECOVERY"][:commented_out] = false
+      data["GRUB_DISABLE_RECOVERY"] = "false"
+      data["GRUB_CMDLINE_LINUX_RECOVERY"] = kernel_params
     end
   end
 end
