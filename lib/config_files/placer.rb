258b9d73b81d8bbce8aa74b3821c2144db5a513c
Josef Reidinger <jreidinger@suse.cz>
2015-11-16 15:34:08 +0100

    if placer not found location, use append behavior

--- lib/config_files/placer.rb
+++ lib/config_files/placer.rb
@@ -10,6 +10,7 @@ module ConfigFiles
   end
 
   # Specialized placer, that allows to place config value before found one.
+  # If noone is found, then append to the end
   # Useful, when config option should be inserted to specific location.
   class BeforePlacer
     def initialize(matcher)
@@ -18,15 +19,19 @@ module ConfigFiles
 
     def new_element(tree)
       index = tree.data.index(&@matcher)
-      raise "Element not found" unless index
 
       res = {}
-      tree.data.insert(index, res)
+      if index
+        tree.data.insert(index, res)
+      else
+        tree.data << res
+      end
       res
     end
   end
 
   # Specialized placer, that allows to place config value after found one.
+  # If noone is found, then append to the end
   # Useful, when config option should be inserted to specific location.
   class AfterPlacer
     def initialize(matcher)
@@ -35,15 +40,19 @@ module ConfigFiles
 
     def new_element(tree)
       index = tree.data.index(&@matcher)
-      raise "Element not found" unless index
 
       res = {}
-      tree.data.insert(index + 1, res)
+      if index
+        tree.data.insert(index + 1, res)
+      else
+        tree.data << res
+      end
       res
     end
   end
 
   # Specialized placer, that allows to place config value instead of found one.
+  # If noone is found, then append to the end
   # Useful, when value already exists and detected by matcher. Then easy add
   # with this placer replace it carefully to correct location.
   class ReplacePlacer
@@ -53,10 +62,14 @@ module ConfigFiles
 
     def new_element(tree)
       index = tree.data.index(&@matcher)
-      raise "Element not found" unless index
-
       res = {}
-      tree.data[index] = res
+
+      if index
+        tree.data[index] = res
+      else
+        tree.data << res
+      end
+
       res
     end
   end
