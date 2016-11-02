73171325641bfc0a1595ca5b1205d47cd6328386
Martin Vidner <mvidner@suse.cz>
2016-11-02 16:49:41 +0100

    Moved AugeasKeysCache to the CFA namespace.

--- lib/cfa/augeas_parser.rb
+++ lib/cfa/augeas_parser.rb
@@ -320,44 +320,44 @@ module CFA
       raise "Augeas parsing/serializing error: #{msg} at #{location}"
     end
   end
-end
 
-# Cache that holds all avaiable keys in augeas tree. It is used to
-# prevent too many aug.match calls which are expensive.
-class AugeasKeysCache
-  STORE_PREFIX = "/store".freeze
+  # Cache that holds all avaiable keys in augeas tree. It is used to
+  # prevent too many aug.match calls which are expensive.
+  class AugeasKeysCache
+    STORE_PREFIX = "/store".freeze
 
-  # initialize cache from passed augeas object
-  def initialize(aug)
-    fill_cache(aug)
-  end
-
-  # returns list of keys available on given prefix
-  def keys_for_prefix(prefix)
-    @cache[prefix] || []
-  end
-
-private
-
-  def fill_cache(aug)
-    @cache = {}
-    search_path = "#{STORE_PREFIX}/*"
-    loop do
-      matches = aug.match(search_path)
-      break if matches.empty?
-      assign_matches(matches, @cache)
-
-      search_path += "/*"
+    # initialize cache from passed augeas object
+    def initialize(aug)
+      fill_cache(aug)
     end
-  end
 
-  def assign_matches(matches, cache)
-    matches.each do |match|
-      split_index = match.rindex("/")
-      prefix = match[0..(split_index - 1)]
-      key = match[(split_index + 1)..-1]
-      cache[prefix] ||= []
-      cache[prefix] << key
+    # returns list of keys available on given prefix
+    def keys_for_prefix(prefix)
+      @cache[prefix] || []
+    end
+
+  private
+
+    def fill_cache(aug)
+      @cache = {}
+      search_path = "#{STORE_PREFIX}/*"
+      loop do
+        matches = aug.match(search_path)
+        break if matches.empty?
+        assign_matches(matches, @cache)
+
+        search_path += "/*"
+      end
+    end
+
+    def assign_matches(matches, cache)
+      matches.each do |match|
+        split_index = match.rindex("/")
+        prefix = match[0..(split_index - 1)]
+        key = match[(split_index + 1)..-1]
+        cache[prefix] ||= []
+        cache[prefix] << key
+      end
     end
   end
 end
