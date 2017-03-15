1f08939916ede50bd1f002750d5d6e136cd70787
Martin Vidner <mvidner@suse.cz>
2017-03-15 11:59:19 +0100

    Isolated the AugeasWriter tests

--- spec/augeas_parser_spec.rb
+++ spec/augeas_parser_spec.rb
@@ -209,71 +209,175 @@ describe CFA::AugeasCollection do
 end
 
 describe CFA::AugeasWriter do
+  let(:parser) { CFA::AugeasParser.new("puppet.lns") }
+  let(:tree) { parser.parse(input_text) }
+  let(:output_text) { parser.serialize(tree) }
+
   describe "#write" do
-    it "writes correctly all modification done on AugeasTree" do
-      test_file = <<DOC
-# comment1
-# comment2
-# comment3
+    context "for collections" do
+      let(:input_text) do
+        <<EOS
+#comment1
+#comment2
+#comment3
+EOS
+      end
 
+      it "deletes a collection item using a matcher" do
+        tree.delete(CFA::Matcher.new(value_matcher: /comment1/))
+        expect(output_text).to eq <<EOS
+#comment2
+#comment3
+EOS
+      end
+
+      it "adds an item before another item" do
+        matcher = CFA::Matcher.new(value_matcher: /comment2/)
+        placer = CFA::BeforePlacer.new(matcher)
+        tree.collection("#comment").add("comment new", placer)
+        expect(output_text).to eq <<EOS
+#comment1
+#comment new
+#comment2
+#comment3
+EOS
+      end
+
+      it "deletes a collection item using #collection" do
+        tree.collection("#comment").delete(/comment2/)
+        expect(output_text).to eq <<EOS
+#comment1
+#comment3
+EOS
+      end
+    end
+
+    context "for an existing subtree" do
+      let(:input_text) do
+        <<EOS
 [main]
-
-test = lest
-to_change = 1 # append comment
+to_change = 1 # trailing comment
 # comment 4
 to_remove = 1
-modified_trailing = 1 # traling to remove
-space_for_trailing =1
-DOC
+EOS
+      end
+      let(:subtree) { tree["main"] }
 
-      expected_output = <<DOC
-# comment new
-# comment3
-
+      it "deletes an item" do
+        subtree.delete("to_remove")
+        expect(output_text).to eq <<EOS
 [main]
-
-test = lest
-added=1
-to_change = 0 # append comment
+to_change = 1 # trailing comment
 # comment 4
-modified_trailing = 1
-space_for_trailing =1#new trailing comment
-[main2]
-#new section
-new_section=1
+EOS
+      end
+
+      it "modifies the value of an AugeasTreeValue" do
+        subtree["to_change"].value = "0"
+        expect(output_text).to eq <<EOS
+[main]
+to_change = 0 # trailing comment
+# comment 4
+to_remove = 1
+EOS
+      end
+
+      it "inserts an item" do
+        placer = CFA::BeforePlacer.new(CFA::Matcher.new(key: "to_remove"))
+        subtree.add("inserted", "1", placer)
+        expect(output_text).to eq <<EOS
+[main]
+to_change = 1 # trailing comment
+# comment 4
+inserted=1
+to_remove = 1
+EOS
+      end
+
+      # WTF, it puts it at the end
+      xit "inserts an item at the 1st position" do
+        placer = CFA::BeforePlacer.new(CFA::Matcher.new(key: "to_change"))
+        subtree.add("inserted", "1", placer)
+        expect(output_text).to eq <<EOS
+[main]
+inserted = 1
+to_change = 1 # trailing comment
+# comment 4
+to_remove = 1
+EOS
+      end
+
+      # WTF, it puts it at the end
+      xit "removes the tree of an AugeasTreeValue" do
+        subtree["to_change"] = "0"
+        expect(output_text).to eq <<EOS
+[main]
+to_change = 0
+# comment 4
+to_remove = 1
+EOS
+      end
+
+      it "changes a value to an AugeasTreeValue" do
+        subtree3 = CFA::AugeasTree.new
+        subtree3["#comment"] = "new trailing comment"
+        tree_value = CFA::AugeasTreeValue.new(subtree3, "2")
+        subtree["to_remove"] = tree_value
+        expect(output_text).to eq <<EOS
+[main]
+to_change = 1 # trailing comment
+# comment 4
+to_remove = 2#new trailing comment
+EOS
+      end
+    end
+
+    context "for a new subtree" do
+      let(:input_text) do
+        <<EOS
+[existing]
+unchanged = boring
+EOS
+      end
+      let(:subtree) do
+        subtree = CFA::AugeasTree.new
+        tree.add("new", subtree)
+        subtree
+      end
+
+      it "adds a collection" do
+        comments = subtree.collection("#comment")
+        comments.add("this is a new section")
+        expect(output_text).to eq <<EOS
+[existing]
+unchanged = boring
+[new]
+#this is a new section
+EOS
+      end
+
+      it "adds an item" do
+        subtree["new_item"] = "1"
+        expect(output_text).to eq <<EOS
+[existing]
+unchanged = boring
+[new]
+new_item=1
+EOS
+      end
+
+      it "adds an AugeasTreeValue" do
+        subtree3 = CFA::AugeasTree.new
+        subtree3["#comment"] = "new trailing comment"
+        tree_value = CFA::AugeasTreeValue.new(subtree3, "1")
+        subtree["trailing_comment"] = tree_value
+        expect(output_text).to eq <<EOS
+[existing]
+unchanged = boring
+[new]
 trailing_comment=1#new trailing comment
-DOC
-
-      parser = CFA::AugeasParser.new("puppet.lns")
-      tree = parser.parse(test_file)
-      tree.delete(CFA::Matcher.new(value_matcher: /comment1/))
-      matcher = CFA::Matcher.new(value_matcher: /comment2/)
-      placer = CFA::BeforePlacer.new(matcher)
-      tree.collection("#comment").add("comment new", placer)
-      tree.collection("#comment").delete(/comment2/)
-      subtree = tree["main"]
-      subtree.delete("to_remove")
-      subtree["to_change"].value = "0"
-      placer = CFA::BeforePlacer.new(CFA::Matcher.new(key: "to_change"))
-      subtree.add("added", "1", placer)
-      subtree["modified_trailing"] = "1"
-      subtree3 = CFA::AugeasTree.new
-      subtree3["#comment"] = "new trailing comment"
-      tree_value = CFA::AugeasTreeValue.new(subtree3, "1")
-      subtree["space_for_trailing"] = tree_value
-
-      # test also adding whole subtree
-      subtree2 = CFA::AugeasTree.new
-      comments = subtree2.collection("#comment")
-      comments.add("new section")
-      subtree2["new_section"] = "1"
-      tree.add("main2", subtree2)
-      subtree3 = CFA::AugeasTree.new
-      subtree3["#comment"] = "new trailing comment"
-      tree_value = CFA::AugeasTreeValue.new(subtree3, "1")
-      subtree2["trailing_comment"] = tree_value
-
-      expect(parser.serialize(tree)).to eq(expected_output)
+EOS
+      end
     end
   end
 end
